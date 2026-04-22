02ccb8b796862c0c94654bbe3b52ce387e9bd121
Gargi Verma <gargi13232@gmail.com>
2026-04-22 14:21:44 +0530

    update

--- React Blog/src/components/Bollywood.jsx
+++ React Blog/src/components/Bollywood.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Data from '../Data'
 
 const Bollywood = () => {
+  const BollywoodData=Data.filter((item)=>item.category==="Bollywood")
+  console.log(BollywoodData)
   return (
-    <div>Bollywood</div>
+    <div>
+      {BollywoodData.map((bollydata)=>(
+        <>
+        <h1>{bollydata.title}</h1>
+        <img src={bollydata.img_url}/>
+        </>
+    ))}
+    </div>
   )
 }
 
--- vite-project-one/src/App.jsx
+++ vite-project-one/src/App.jsx
@@ -1,4 +1,4 @@
-// import React from 'react'   
+import React from 'react'   
 // // import Navbar from './Navbar'
 // // import "./App.css"
 // // import {add,sub} from "./components/Math"
